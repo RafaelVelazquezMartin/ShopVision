11ac64b49c8a4d7fb49bee98f51ae156470c684d
hopedoolan <hope.doolan@yahoo.com>
2018-03-08 16:34:41 +0000

    added buttons

--- ShopVision/app/src/main/java/com/example/shopvision/MainActivity.java
+++ ShopVision/app/src/main/java/com/example/shopvision/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         mNextButton = findViewById(R.id.basic_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
